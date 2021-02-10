7eba67d03babed060b9924b6993c1e4c69752917
Arthur <arthmayer@outlook.fr>
2021-02-10 13:50:16 +0000

    add q3 q4

---
+++ models/base_model.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+This module is the Base Model module
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    BaseModel class
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        constructor
+            args:
+                args (int): int
+                kwargs: assign
+        """
+        if kwargs:
+            self.id = kwargs["id"]
+            self.my_number = kwargs["my_number"]
+            self.name = kwargs["name"]
+            self.created_at = datetime.strptime(kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        str magic method
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        updates the public instance attribute updated_at with the current datetime
+        """
+        date = datetime.now()
+        date = date.isoformat()
+        self.update_at = date
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of __dict__ of the instance
+        """
+        dic = {
+                "id": self.id,
+                "updated_at": self.updated_at.isoformat(),
+                "created_at": self.created_at.isoformat(),
+                "name": self.name,
+                "my_number": self.my_number,
+                "__class__": self.__class__.__name__
+                }
+        return dic
