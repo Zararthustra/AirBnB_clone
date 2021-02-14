71c9ebe5174d1a718f8a515ea6763becd0c21976
Arthur <arthmayer@outlook.fr>
2021-02-14 16:18:24 +0000

    add all, update and destroy

--- console.py
+++ console.py
@@ -19,6 +19,31 @@ class HBNBCommand(cmd.Cmd):
     __file_path = "file.json"
     classes = [ "BaseModel" ]
 
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or updating attribute
+        """
+        args = arg.split()
+        if args == [] or arg == "":
+            print("** class name missing **")
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        elif (args[0] + "." + args[1]) not in storage.all():
+            print("** no instance found **")
+        elif len(args) < 3:
+            print("** attribute name missing **")
+        elif len(args) < 4:
+            print("** value missing **")
+        else:
+            stor_a = storage.all()
+            key = args[0] + "." + args[1]
+            setattr(stor_a[key], args[2], args[3])
+            storage.save()
+            
+
+
     def do_create(self, arg):
         """
         Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id.
@@ -36,12 +61,21 @@ class HBNBCommand(cmd.Cmd):
         """
         Deletes an instance based on the class name and id
         """
-        if arg == "":
+        args = arg.split()
+        if args == [] or arg == "":
             print("** class name missing **")
-        if arg not in self.classes:
+        elif args[0] not in self.classes:
             print("** class doesn't exist **")
-        if arg == "i":
-            print()
+        elif len(args) < 2:
+            print("** instance id missing **")
+        elif (args[0] + "." + args[1]) not in storage.all():
+            print("** no instance found **")
+        else:
+            key = args[0] + "." + args[1]
+            stor_a = storage.all()
+            if key in stor_a:
+                del stor_a[key]
+                storage.save()
 
     def do_show(self, arg):
         """
@@ -61,6 +95,25 @@ class HBNBCommand(cmd.Cmd):
             stor_a = storage.all()
             if key in stor_a:
                 print(stor_a[key])
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances based or not on the class name
+        """
+        stor_a = storage.all()
+        obj_list = []
+        if arg:
+            if arg not in self.classes:
+                print("** class doesn't exist **")
+            else:
+                for key in stor_a:
+                    class_id = key.split(".")
+                    if class_id[0] == arg:
+                        obj_list.append(stor_a[key].__str__())
+        else:
+            for key in stor_a:
+                obj_list.append(stor_a[key].__str__())
+        print(obj_list)
 
     def do_help(self, arg):
         """
