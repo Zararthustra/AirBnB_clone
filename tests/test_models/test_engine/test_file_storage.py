003412f99960ee839fc1e7be771ed9fd488ef1e3
gchuinard <gautier.chuinard@lilo.org>
2021-02-15 16:24:04 +0000

    add unittest engine

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+FileStorage unittest
+"""
+
+import unittest
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+import os
+
+
+class TestFileStorage(unittest.TestCase):
+    """
+    FileStorage unittest
+    """
+
+    def test_class(self):
+        """
+        test if the object is a type of FileStorage
+        """
+
+        obj = FileStorage()
+        self.assertEqual(type(obj), FileStorage)
+
+    def test_attr(self):
+        """
+        test attributs
+        """
+
+        obj = FileStorage()
+        self.assertEqual(obj._FileStorage__file_path, "file.json"),
+        self.assertEqual(type(obj._FileStorage__objects), dict),
+
+    def test_save(self):
+        """
+        test if the method save() save the object in JSON file
+        """
+
+        obj = BaseModel()
+        obj.name = "Tutur"
+        obj.my_number = 29
+        obj.save()
+        self.assertTrue(os.path.exists("file.json"))
+
+
